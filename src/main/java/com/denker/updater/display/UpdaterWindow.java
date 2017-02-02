6d08ee62ffcaacd1355654090a8db694352c31f2
denkers <kyle.business@inbox.com>
2017-02-02 20:53:28 +1300

    Updated display.UpdaterWindow with init of frames and panel: display.UpdaterWindow and temporary main

--- src/main/java/com/denker/updater/display/UpdaterWindow.java
+++ src/main/java/com/denker/updater/display/UpdaterWindow.java
@@ -6,15 +6,45 @@
 
 package com.denker.updater.display;
 
+import javax.swing.JFrame;
+
 public class UpdaterWindow 
 {
     private static UpdaterWindow instance;
+    private JFrame frame;
+    private UpdaterPanel panel;
     
-    private UpdaterWindow() {}
+    private UpdaterWindow() 
+    {
+        initDisplay();
+    }
+    
+    private void initDisplay()
+    {
+        frame   =   new JFrame();
+        panel   =   UpdaterPanel.getInstance();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+    
+    public void display()
+    {
+        if(frame != null)
+            frame.setVisible(true);
+    }
     
     public static UpdaterWindow getInstance()
     {
         if(instance == null) instance = new UpdaterWindow();
         return instance;
+    }
+    
+    public static void main(String[] args)
+    {
+        UpdaterWindow window    =   UpdaterWindow.getInstance();
+        window.display();
     }
 }
